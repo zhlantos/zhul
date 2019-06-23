8c13219362e04c7e2520dbca6194bb21a7603e30
zhlantos <51984728+zhlantos@users.noreply.github.com>
2019-06-23 07:18:48 +0700

    Update beta.py

--- beta.py
+++ beta.py
@@ -543,8 +543,8 @@ def super():
     print 40 * '\x1b[1;97m\xe2\x95\x90'
     print '\x1b[1;37;40m1. Crack dari daftar Teman'
     print '\x1b[1;37;40m2. Crack dari member Grup'
-    print '\x1b[1;31;40m3. Crack dari Friend Teman'
-    print '\x1b[1;37;40m0. Kembali'
+    print '\x1b[1;37;40m3. Crack dari Friend Teman'
+    print '\x1b[1;31;40m0. Kembali'
     pilih_super()
 
 
@@ -589,7 +589,7 @@ def pilih_super():
 					os.system('clear')
 					print logo
 					print 40 * '\x1b[1;97m\xe2\x95\x90'
-					idg = raw_input('\x1b[1;91m[+] \x1b[1;92mMasukan ID Teman   \x1b[1;91m:\x1b[1;97m ')
+					idg = raw_input('\x1b[1;91m[+] \x1b[1;92mInput ID friend \x1b[1;91m: \x1b[1;97m')
 					try:
 						r = requests.get('https://graph.facebook.com/' + idg + '$access_token=' + toket)
 						asw = json.loads(r.text)
